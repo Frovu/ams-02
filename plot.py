89db9ce59eb9a299508a3f2eba0cd722dbe20195
Frovy <sfrovis@gmail.com>
2020-08-18 03:30:27 +0300

    plot errorbars

--- plot.py
+++ plot.py
@@ -21,14 +21,18 @@ def get_spectrum(data, time):
 	lines = [l for l in data if time in l[1]]
 	flux = [l[5] for l in lines]
 	rigidity = [(l[3]+l[4])/2 for l in lines]
-	return flux, rigidity
+	errors = [(l[6]+l[7]+l[8]+l[9])/2 for l in lines]
+	errors = np.array(errors) * 3
+	return flux, rigidity, errors
 
 fig, ax = plt.subplots()
 ax.set_ylabel('flux')
 ax.set_xlabel('R, GV')
-def plot_one(flux, rigidity, color, label):
+def plot_one(flux, rigidity, errors, color, label):
 	#ax.plot(rigidity, flux, f'{color}.', label=label)
-	ax.loglog(rigidity, flux, f'{color}.', label=label)
+	ax.set_xscale('log')
+	ax.set_yscale('log')
+	ax.errorbar(rigidity, flux, fmt=f'{color}.', yerr=errors, label=label)
 	#ax.xaxis.set_major_locator(MultipleLocator(base=10))
 	#ax.grid()
 
@@ -36,14 +40,14 @@ def g(r, param):
 	return 10000 * (r ** (param))
 
 def plot_model(data, time, param, c):
-	f, rigidity = get_spectrum(data, time)
+	f, rigidity, e = get_spectrum(data, time)
 	flux = [g(r, param) for r in rigidity]
 	ax.loglog(rigidity[1:], flux[1:], c, label='model')
 	#ax.loglog(rigidity, flux, 'w', label='model')
 
 def get_and_plot(data, time, color, label):
-	f, r = get_spectrum(data, time)
-	plot_one(f, r, color, label)
+	f, r, e = get_spectrum(data, time)
+	plot_one(f, r, e, color, label)
 
 def find_date(data, date):
 	for r in data:
